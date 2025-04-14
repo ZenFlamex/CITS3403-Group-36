bf2eb963d9a0b80429c38820c57c7c0c03fe3dd5
breg11 <23152493@student.uwa.edu.au>
2025-04-14 22:51:55 +0800

    Add signup, login, forgot password pages; update styling and logo

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,16 @@ from flask import render_template
 @application.route('/')
 def index():
     return render_template('index.html')
+
+@application.route('/signup.html') 
+def signup():
+    return render_template('signup.html')
+
+@application.route('/login.html')
+def login():
+    return render_template('login.html') 
+
+
+@application.route('/forgot_password.html')
+def forgot_password():
+    return render_template('forgot_password.html') 
