a8e2260936209fb88e7da4b0fdb925e4e31458fa
breg11 <23152493@student.uwa.edu.au>
2025-04-25 16:28:00 +0800

    feat: Refactor login with Flask-WTF and Flask-Login

--- app/models.py
+++ app/models.py
@@ -2,21 +2,35 @@ from datetime import datetime
 from typing import Optional, List
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_manager
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-    is_authenticated: so.Mapped[bool] = so.mapped_column(sa.Boolean, default=True)
+    
     
     # Relationships
     books: so.Mapped[List["Book"]] = so.relationship(back_populates="creator")
     notifications_received: so.Mapped[List["Notification"]] = so.relationship(back_populates="receiver")
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        if self.password_hash is None:
+            return False
+        return check_password_hash(self.password_hash, password)
     
     def __repr__(self):
         return f'<User {self.username}>'
+
+@login.user_loader
+def load_user(id):
+   return db.session.get(User, int(id))
 
 class Book(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
